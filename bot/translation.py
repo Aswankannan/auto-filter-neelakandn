6e212b8cf78fb2846cf24e167e584998ebebd6a7
Aswankannan <88327644+Aswankannan@users.noreply.github.com>
2021-08-21 12:02:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,9 @@ class Translation(object):
 എന്നെ നിനക്ക് നിൻ്റെ ഗ്രൂപ്പിൽ ചേർക്കാൻ പറ്റില്ല ഇനി അത് നോക്കി നീ വേരണ്ട
 
 </i>"""    
-    
+    HELP_TEXT = """<b> എന്താടാ നിൻ്റെ കൊഴപ്പം </b>
+<b> മരിയാതയ്ക്ക് സ്ഥലം വിട്ടോ എൻ്റെ സ്വഭാവം മറ്റൽ നീ</b>
+"""
 
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> MANGALASSERI NEELAKANDAN</code>
